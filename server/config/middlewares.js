5880402ad6249f029edb3686a03699a2e94ef6f6
qian wang <1510106069@qq.com>
2023-02-09 14:38:09 +0800

    feat: 处理数据

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', 'http://localhost:1337'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
--- server/src/api/layout/controllers/layout.js
+++ server/src/api/layout/controllers/layout.js
@@ -3,7 +3,17 @@
 /**
  * layout controller
  */
+const { removeTime, removeAttrsAndId } = require('../../../utils/index');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::layout.layout');
+module.exports = createCoreController('api::layout.layout', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: 'deep',
+    };
+    const { data } = await super.find(ctx);
+    return removeAttrsAndId(removeTime(data[0]));
+  }
+}));
